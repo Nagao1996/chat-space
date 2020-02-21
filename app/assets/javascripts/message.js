ba12877cfff56308e993706e84e1a765215fbe7d
Nagao1996 <issei.517.nagao@gmail.com>
2020-02-21 18:03:34 +0900

    deplkoy

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,49 +1,49 @@
 $(function(){
-    function buildHTML(message){
-      if ( message.image ) {
-        var html =
-          `<div class="message" data-message-id=${message.id}>
-            <div class="message-list__chat-box">
-              <div class="message-list__chat-box__user-box">
-                <div class="message-list__chat-box__user-box__user-name">
-                  ${message.user_name}
-                </div>
-                <div class="message-list__chat-box__user-box__date">
-                  ${message.created_at}
-                </div>
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message-list__chat-box">
+            <div class="message-list__chat-box__user-box">
+              <div class="message-list__chat-box__user-box__user-name">
+                ${message.user_name}
               </div>
-              <div class="message-list__chat-box__chat-message">
-                <p class="message-list__chat-box__chat-message__content">
-                  ${message.content}
-                </p>
+              <div class="message-list__chat-box__user-box__date">
+                ${message.created_at}
               </div>
-              <img src=${message.image} >
             </div>
-          </div>`
-        return html;
+            <div class="message-list__chat-box__chat-message">
+              <p class="message-list__chat-box__chat-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>
+        </div>`
+      return html;
         
-      } else {
-        var html =
-          `<div class="message" data-message-id=${message.id}>
-            <div class="message-list__chat-box">
-              <div class="message-list__chat-box__user-box">
-                <div class="message-list__chat-box__user-box__user-name">
-                  ${message.user_name}
-                </div>
-                <div class="message-list__chat-box__user-box__date">
-                  ${message.created_at}
-                </div>
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message-list__chat-box">
+            <div class="message-list__chat-box__user-box">
+              <div class="message-list__chat-box__user-box__user-name">
+                ${message.user_name}
               </div>
-              <div class="message-list__chat-box__chat-message">
-                <p class="message-list__chat-box__chat-message__content">
-                  ${message.content}
-                </p>
+              <div class="message-list__chat-box__user-box__date">
+                ${message.created_at}
               </div>
             </div>
-          </div>`
-        return html;
-      };
-    }
+            <div class="message-list__chat-box__chat-message">
+              <p class="message-list__chat-box__chat-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
@@ -56,17 +56,18 @@ $(function(){
         processData: false,
         contentType: false
       })
-        .done(function(data){
-          var html = buildHTML(data);
-          $('.messages').append(html);
-          $('form')[0].reset();
-        })
-        .fail(function() {
-          alert("メッセージの送信に失敗しました。");
-        })
-        .always(function(){
-          $(".submit-btn").prop("disabled", false);
-        });
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('form')[0].reset();
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      })
+      .fail(function() {
+        alert("メッセージの送信に失敗しました。");
+      })
+      .always(function(){
+        $(".submit-btn").prop("disabled", false);
+      });
   })
 
     var reloadMessages = function() {
@@ -78,13 +79,12 @@ $(function(){
         data: {id: last_message_id}
       })
       .done(function(messages) {
-        if (messages.length !== 0) { 
+        if (messages.length !== 0) {
           var insertHTML = '';
           $.each(messages, function(i, message) {
             insertHTML += buildHTML(message)
           });
           $('.messages').append(insertHTML);
-          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
         }
       })
       .fail(function() {
@@ -97,5 +97,3 @@ $(function(){
       setInterval(reloadMessages, 7000);
     }
 });
-
-
